c45d46089fa885300cfe0ceb83abc031114883bb
Jason Peleman <jason.peleman@student.ucll.be>
2026-04-22 15:04:26 +0200

    database, player update

--- Webapp/RaceCore latest/RaceCore/RaceCore/StageProject_RaceCore/Models/Player.cs
+++ Webapp/RaceCore latest/RaceCore/RaceCore/StageProject_RaceCore/Models/Player.cs
@@ -6,8 +6,13 @@
 
         public string Name { get; set; } = string.Empty;
 
-        public List<PlayerSelection> Selections { get; set; } = new();
-        public List<DraftTurn> DraftTurns { get; set; } = new();
-        public List<PlayerPoints> PlayerPoints { get; set; } = new();
+        public int PositionInDraft { get; set; }
+
+        public int TotalPoints { get; set; } = 0;
+
+        public ICollection<PlayerSelection> Selections { get; set; } = new List<PlayerSelection>();
+
+        public ICollection<DraftTurn> DraftTurns { get; set; } = new List<DraftTurn>();
+        public ICollection<PlayerPoints> PlayerPoints { get; set; } = new List<PlayerPoints>();
     }
 }
